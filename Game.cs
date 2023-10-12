b0316832795b22ced6b17387a9da3048319f7110
Vitória Tenório <48605624+vitorialira92@users.noreply.github.com>
2023-10-12 18:02:35 -0300

    Adicionar arquivos de projeto.

---
+++ Game.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    public class Game
+    {
+        private long number;
+        private decimal numberLevelFive;
+        private int level;
+        private int points;
+        private int attempts;
+
+
+        public Game()
+        {
+            this.points = 0;
+        }
+
+
+        public bool game()
+        {
+            bool correctLevel = false;
+
+            do
+            {
+                Console.WriteLine("> Choose your level from 1 to 5! Make sure to type right or we'll ask again :)");
+                correctLevel = int.TryParse(Console.ReadLine(), out level);
+                correctLevel = correctLevel ? (level >= 1 && level <= 5) : false;
+            } while (!correctLevel);
+
+            this.generateNumber(); //attempts here
+
+            if (level == 5)
+                Console.WriteLine(">>> The number has 4 decimals");
+
+
+
+            int op; bool validation = false;
+            bool won = false;
+
+            while (attempts > 0)
+            {
+                do
+                {
+                    getOptions();
+                    Console.WriteLine($"You have {attempts} attempts !!");
+                    Console.WriteLine("Type your option!");
+                    validation = int.TryParse(Console.ReadLine(), out op);
+                    Console.WriteLine("------------------------------------------------------------------------\n\n\n");
+                    validation = validation ? (op > 0 && op <= 3) : false;
+                } while (!correctLevel);
+
+                if (op == 3)
+                    won = (level == 5) ? takeAGuessLevelFive() : takeAGuess();
+                else
+                    this.chooseOption(op);
+                
+                Console.WriteLine("------------------------------------------------------------------------\n\n\n");
+
+                if (won)
+                {
+                    Console.WriteLine($"You accumulated {points} points!");
+                    Console.WriteLine("------------------------------------------------------------------------\n\n\n");
+
+                    return true;
+                }
+            }
+
+            Console.Clear();
+            if (!won)
+                Console.WriteLine("Your attempts are over :/");
+            Console.WriteLine($"You accumulated {points} points!");
+            Console.WriteLine("But... you also lost... Your number was.... ");
+            Console.Write((level == 5) ? numberLevelFive.ToString() : number.ToString());
+            Console.WriteLine("------------------------------------------------------------------------\n\n\n");
+
+            return false;
+        }
+
+        public void getOptions()
+        {
+            string option1 = "1 - Check if the number is even\n";
+            string option2 = (level == 5) ? ("2 - Find the nearest multiple of 100\n") : ("2 - Know if it is positive or negative\n");
+            string option3 = "3 - Guess the number\n";
+            Console.WriteLine(option1 + option2 + option3);
+        }
+
+        public void chooseOption(int op)
+        {
+            Console.Clear();
+            decimal multiple = Math.Round(numberLevelFive / 100) * 100;
+
+            string option = "";
+            if (op == 1)
+                option = ((level == 5)? (numberLevelFive % 2 == 0): (number %2 == 0)) ? "It is even" : "It is not even";
+            else if (op == 2)
+                option = (level == 5) ? $"\nThe nearest 100 multiple is {multiple}\n" : (number >= 0) ? "It is positive" : "It is negative";
+           
+            
+            Console.WriteLine(option);
+        }
+
+        private bool takeAGuess()
+        {
+
+            Console.WriteLine("Type your guess");
+            long guess = 0;
+            long.TryParse(Console.ReadLine(), out guess);
+
+            if (guess == number)
+            {
+                Console.Clear();
+                Console.WriteLine($"Congratulations!! You guessed right in the level {level} : {number}\n");
+                Console.WriteLine("You get " + level * 5 + " points !!");
+                points += level * 5;
+                attempts = 0;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You did not guess it right but here's a tip for you:");
+                int difference = (int)Math.Abs(number - guess);
+                difference = difference / 10;
+                Console.WriteLine($"Your guess and the right answer are exactly {difference} dozens of integers away.\n");
+                if (difference <= 2)
+                {
+                    Console.WriteLine("Due to how close you got from the right number, you get 1 points :)");
+                    points += 1;
+                }
+                attempts--;
+                return false;
+                
+            }
+
+        }
+
+        private bool takeAGuessLevelFive()
+        {
+            Console.WriteLine("Type your guess");
+            decimal guess = 0;
+            decimal.TryParse(Console.ReadLine(), out guess);
+
+
+
+            if (guess == numberLevelFive)
+            {
+                Console.Clear();
+                Console.WriteLine("Congratulations!! You guessed right in the hardest level");
+                Console.WriteLine("You get " + 100 + " points !!");
+                points += 100;
+                attempts = 0;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You did not guess it right but here's a tip for you:");
+                long difference = (long) Math.Abs(Math.Abs(numberLevelFive) - Math.Abs(guess));
+                Console.WriteLine($"Your guess and the right answer are exactly {difference} integers away.\n");
+                if (difference == 0)
+                {
+                    string n1 = numberLevelFive.ToString();
+                    string n2 = guess.ToString();
+
+                    int count = n2.Length;
+                    for (int i = 0; i < n1.Length; i++)
+                    {
+                        if ( i < n2.Length)
+                        {
+                            if (n1[i] == n2[i])
+                                count--;
+                        }
+                    }
+                    Console.WriteLine($"To help a bit more, the answer you gave has {(Math.Abs(count))} wrong digits");
+                    Console.WriteLine("AND due to how close you got from the right number, you get 7 points :)");
+                    Console.WriteLine();
+                    points += 7;
+                }
+                else if(difference <= 1000)
+                {
+                    Console.WriteLine("Due to how close you got from the right number, you get 5 points :)");
+                    points += 5;
+                }
+                
+                attempts--;
+                return false;
+            }
+        }
+
+        private void generateNumber()
+        {
+            Random random = new Random();
+
+            if (level == 5)
+            {
+                decimal limiteInferior = (decimal)-Math.Pow(100.0, double.Parse(level.ToString()));  
+                decimal limiteSuperior = limiteInferior * (-1); 
+
+                attempts = 38;
+
+                Console.WriteLine($"Your number is between {limiteInferior} and {limiteSuperior}");
+                Console.WriteLine($"You have {attempts} attempts.");
+
+
+                numberLevelFive = (decimal)(random.NextDouble() * ((double)limiteSuperior - (double)limiteInferior) + (double)limiteInferior);
+
+                numberLevelFive = Math.Round(numberLevelFive, 4);
+
+            }
+            else
+            {
+                long limiteInferior = (long)-Math.Pow(100.0, double.Parse(level.ToString()));
+                long limiteSuperior = limiteInferior * (-1); 
+
+                attempts = (int)Math.Pow(level, 3) + 5;
+
+                number = (long)(random.NextDouble() * (limiteSuperior - limiteInferior) + limiteInferior);
+                Console.WriteLine($"Your number is between {limiteInferior} and {limiteSuperior}");
+                Console.WriteLine($"You have {attempts} attempts.");
+
+            }
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+using GuessTheNumber;
+using System;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        introduction();
+
+        string name = Console.ReadLine();
+        Console.WriteLine($"Ok, {name}, let's start!");
+
+        play(name);
+
+        Console.WriteLine("See you again soon ;)");
+
+    }
+
+    private static void introduction()
+    {
+        Console.WriteLine("Oh, hey there! Welcome to guess the number!");
+        Console.WriteLine("I will very quickly explain the game.\n");
+        Console.WriteLine("Well, pretty much you have to guess the number we have here =D");
+        Console.WriteLine("There are five levels from 1 to 5: 1 - ezpz\n2 - still ezpz\n" +
+            "3 - ok...\n4 - hard.\n5 - simply impossible");
+        Console.WriteLine("\nDon't worry, we'll help you through it\n\n");
+        Console.WriteLine("May you tell us your name?");
+    }
+
+    private static void play(string name)
+    {
+        int level;
+        int op = 1;
+        Game gameObject = new Game();
+        bool correctTyping = false;
+        bool playing = true;
+        int partidas = 0;
+        int points = 0;
+
+
+        while (playing)
+        {
+            if(partidas > 0)
+            {
+                do
+                {
+                    Console.WriteLine("Type 1 to keep playing and 0 to finish the game");
+                    correctTyping = int.TryParse(Console.ReadLine(), out op);
+                    correctTyping = correctTyping ? (op == 0 || op == 1) : false;
+                } while (!correctTyping);
+
+                if(op == 0)
+                {
+                    return;
+                }
+            }
+           
+           gameObject.game();
+           partidas++;
+        
+        
+        }
+
+
+        
+
+        
+    }
+
+}
